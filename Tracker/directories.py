05953ebd3e8a6dbf8d86d33094f17152606d7dc9
D-Hastie <dhastie@live.com>
2017-02-03 14:37:35 +0000

    update

--- Tracker/directories.py
+++
@@ -1,4 +0,0 @@
-datafiledir = '/Users/Fuck/Documents/AmazonTracker/Datafiles/'
-pdfdir ='/Users/Fuck/Documents/AmazonTracker/Tracker/PDFs/'
-AttachmentLoc = '/Users/Fuck/Documents/AmazonTracker/EmailChecker/MailAttach/'
-trackerdir = '/Users/Fuck/Documents/AmazonTracker/Tracker/'
